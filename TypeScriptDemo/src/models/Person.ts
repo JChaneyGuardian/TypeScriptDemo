b463a5a28da3d13d7bdb2f645786e2302bb4aa61
Jeff Chaney <jchaney@guardian.com>
2017-09-25 13:08:28 -0400

    Added namespaces

--- TypeScriptDemo/src/models/Person.ts
+++ TypeScriptDemo/src/models/Person.ts
@@ -1,27 +1,29 @@
 ﻿// Person
-class Person implements IPerson {
-    lastName: string;
-    firstName: string;
+namespace Identity {
+    export class Person implements IPerson {
+        lastName: string;
+        firstName: string;
 
-    constructor(firstName: string, lastName: string) {
-        this.firstName = firstName;
-        this.lastName = lastName;
+        constructor(firstName: string, lastName: string) {
+            this.firstName = firstName;
+            this.lastName = lastName;
+        }
+
+        FullName = () => {
+            return this.firstName + " " + this.lastName;
+        }
     }
 
-    FullName=()=> {
-        return this.firstName + " " + this.lastName;
+    export class PersonWithMiddleName extends Person {
+        middleName: string;
+        constructor(firstName: string, middleName: string, lastName: string) {
+            super(firstName, lastName);
+            this.middleName = middleName;
+        }
+
+        FullName = () => {
+            return this.firstName + " " + this.middleName + " " + this.lastName;
+        }
+
     }
 }
-
-class PersonWithMiddleName extends Person {
-    middleName: string;
-    constructor(firstName: string, middleName: string, lastName: string) {
-        super(firstName, lastName);
-        this.middleName = middleName;
-    }
-
-    FullName = () => {
-        return this.firstName + " " + this.middleName + " " + this.lastName;
-    }
-
-}	
--- TypeScriptDemo/src/tests/GreetingTest.ts
+++ TypeScriptDemo/src/tests/GreetingTest.ts
@@ -2,45 +2,45 @@
 function setup_tests() {
     describe('Greeting function', () => {
         it('should return both first and last name', () => {
-            var result = greeting("Test", "Person");
+            var result = Lib.greeting("Test", "Person");
             chai.expect(result).to.equal("Hello Test Person!");
         })
         it('should be able to use an adhoc person', () => {
-            var person: IPerson = {
+            var person: Identity.IPerson = {
                 lastName: "Dog",
                 middleName: "The",
                 firstName: "Snoop",
                 FullName() { return "Martha Stewart"; }
             }
-            var result = greeting(person);
+            var result = Lib.greeting(person);
             chai.expect(result).to.equal("Hello Martha Stewart!");
         })
 
         it('should be able to use a Person object', () => {
-            var person = new Person("Uncle", "Bob");
+            var person = new Identity.Person("Uncle", "Bob");
 
-            var result = greeting(person);
+            var result = Lib.greeting(person);
             chai.expect(result).to.equal("Hello Uncle Bob!");
         });
 
         it('should be able to use a PersonWithMiddleName object', () => {
-            var person = new PersonWithMiddleName("Sam", "I", "Am");
+            var person = new Identity.PersonWithMiddleName("Sam", "I", "Am");
 
-            var result = greeting(person);
+            var result = Lib.greeting(person);
             chai.expect(result).to.equal("Hello Sam I Am!");
         })
 
         it('should be able to use a SecurityIdentity object', () => {
             var person = new SecurityIdentity("Sam", "I", "Am", undefined);
 
-            var result = greeting(person);
+            var result = Lib.greeting(person);
             chai.expect(result).to.equal("Hello Sam I Am!");
         })
 
         it('should be able to handled an undefined', () => {
             //var person = new PersonWithMiddleNameObj("Sam", "I", "Am");
 
-            var result = greeting(undefined);
+            var result = Lib.greeting(undefined);
             chai.expect(result).to.equal(undefined);
         })
     })
